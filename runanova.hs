abe24e1fa2f5210161d38ef6156781f398b35971
Etadel II <prof.etadel2@gmail.com>
2011-04-21 15:13:53 -0430

    Cambiados los sets de prueba.

--- runanova.hs
+++ runanova.hs
@@ -14,19 +14,18 @@ main = do
         1 -> do
             putStrLn "-- Algoritmo: GA."
             runAnova (genGA 3) "GA" 30
-            putStrLn "-- Algoritmo: Bee."
-            runAnova (genBee 3) "Bee" 30
+            putStrLn "-- Algoritmo: PSO."
+            runAnova (genPSO 3 (255.0, 255.0, 255.0) (0.0,0.0,0.0)) "PSO" 30
+            putStrLn "-- Algoritmo: WPSO."
+            runAnova (genWPSO 3 (255.0, 255.0, 255.0) (0.0,0.0,0.0)) "WPSO" 30
         2 -> do
             putStrLn "-- Algoritmo: DE."
             runAnova (genDE 20 (255.0, 255.0, 255.0) (0.0,0.0,0.0)) "DE" 30
             putStrLn "-- Algoritmo: SDE."
             runAnova (genSDE 20 (255.0, 255.0, 255.0) (0.0,0.0,0.0)) "SDE" 30
+            putStrLn "-- Algoritmo: Bee."
+            runAnova (genBee 3) "Bee" 30
         3 -> do
-            putStrLn "-- Algoritmo: PSO."
-            runAnova (genPSO 3 (255.0, 255.0, 255.0) (0.0,0.0,0.0)) "PSO" 30
-            putStrLn "-- Algoritmo: WPSO."
-            runAnova (genWPSO 3 (255.0, 255.0, 255.0) (0.0,0.0,0.0)) "WPSO" 30
-        4 -> do
             putStrLn "-- Algoritmo: Ant."
             runAnova (genAnt 3) "Ant" 30
         _ -> do
