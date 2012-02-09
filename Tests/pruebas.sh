65bbf60fcc1bc773611ad45276685a4a2fc504ca
Federico Ponte <fedep3@gmail.com>
2012-02-09 00:10:01 -0500

    Arreglando varias cosas

--- Tests/pruebas.sh
+++ Tests/pruebas.sh
@@ -14,13 +14,10 @@ rm .log 2> /dev/null
 
 for (( i=1; i<5; i++ ))
 do
-    mkdir GAL${lc[${i}]}
     for (( j=1; j<31; j++ ))
     do
         echo "GA LENNA CLUSTERS ${lc[${i}]} CORRIDA ${j}"
         ./runTests GAL${lc[${i}]} TestsPublicacion/galenna${lc[${i}]}.test
-        rename "s/GA0-imglennamediumtiff/GAL${lc[${i}]}${j}/" *.tiff
-        mv *.tiff GAL${lc[${i}]}
         ../Parser/parser TGA ../Parser/GAL${lc[${i}]}.sql GAL${lc[${i}]}.result
         rm .log
     done
@@ -28,36 +25,27 @@ done
 
 for (( i=1; i<5; i++ ))
 do
-    mkdir GAP${pc[${i}]}
     for (( j=1; j<31; j++ ))
     do
         echo "GA PEPPERS CLUSTERS ${pc[${i}]} CORRIDA ${j}"
         ./runTests GAP${pc[${i}]} TestsPublicacion/gapepper${pc[${i}]}.test
-        rename "s/GA0-imgpeppersmediumtiff/GAP${pc[${i}]}${j}/" *.tiff
-        mv *.tiff GAP${pc[${i}]}
         ../Parser/parser TGA ../Parser/GAP${pc[${i}]}.sql GAP${pc[${i}]}.result
         rm .log
     done
 done
 
-mkdir BEEL${lc[1]}
 for (( j=1; j<31; j++ ))
 do
     echo "BEE LENNA CLUSTERS ${lc[1]} CORRIDA ${j}"
     ./runTests BEEL${lc[1]} TestsPublicacion/beelenna${lc[1]}.test
-    rename "s/Bee0-imglennamediumtiff/BEEL${lc[1]}${j}/" *.tiff
-    mv *.tiff BEEL${lc[1]}
     ../Parser/parser TBee ../Parser/BEEL${lc[1]}.sql BEEL${lc[1]}.result
     rm .log
 done
 
-mkdir BEEP${lc[1]}
 for (( j=1; j<31; j++ ))
 do
     echo "BEE PEPPERS CLUSTERS ${lc[1]} CORRIDA ${j}"
     ./runTests BEEP${lc[1]} TestsPublicacion/beepepper${lc[1]}.test
-    rename "s/Bee0-imgpeppersmediumtiff/BEEP${lc[1]}${j}/" *.tiff
-    mv *.tiff BEEP${lc[1]}
     ../Parser/parser TBee ../Parser/BEEP${lc[1]}.sql BEEP${lc[1]}.result
     rm .log
 done
@@ -66,6 +54,6 @@ cd ..
 cd Parser
 ./genPubTables.py
 pdflatex tablas.tex
-mv table* tables
+mv table.*.tex tables
 cd ..
 cd Tests
