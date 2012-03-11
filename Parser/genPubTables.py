4f7019ef0227e350d44782979c0a6746593a84c0
Federico Ponte <fedep3@gmail.com>
2012-03-10 19:36:57 -0500

    Cambien el script para agregarle el intervalo y le quite cosas a las tablas

--- Parser/genPubTables.py
+++ Parser/genPubTables.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import sqlite3
+import math
+from decimal import Decimal
 
 def genSpaces(lenght):
     spaces = ''
@@ -35,59 +37,39 @@ def insertion_sort(lista):
 def compL(felem,selem):
     if felem[0] < selem[0]:
         return True
-    elif felem[0] == selem[0] and felem[1] >= selem[1]:
-        return True
     else:
         return False
         
-def insertion_sortE(lista):
-    for i in range(len(lista)):
-        save = lista[i]
-        j = i
-        while j > 0 and compLE(lista[j - 1], save):
-            lista[j] = lista[j - 1]
-            j -= 1
-            lista[j] = save
-        
-def compLE(felem,selem):
-    if felem[0] > selem[0]:
-        return True
-    elif felem[0] == selem[0] and felem[1] >= selem[1]:
-        return True
-    else:
-        return False
-
-def takeOut(table, parametros, nclust, avg, maxs, mins, params, latexTable, start, end):
+def takeOut(table, parametros, nclust, statistics, maxs, mins, params, latexTable, start, end):
     for i in range(start,end):
 
-        consulta = 'SELECT ROUND(AVG('+table[0]+'_hib_fo),4), ROUND(AVG('+table[0]+'_hib_db),4), ROUND(AVG('+table[0]+'_hib_je),4), ROUND(AVG('+table[0]+'_hib_eval),4), ROUND(AVG('+table[0]+'_hib_time),4), ROUND(AVG('+table[0]+'_cluster_f),4) FROM '+table[1]+' WHERE '+table[0]+'_cluster_f >= '+nclust+' AND '+table[0]+'_type = '+str(i+1)
-        avg.execute(consulta)
-        consulta = 'SELECT '+table[0]+'_hib_fo, '+table[0]+'_hib_db, '+table[0]+'_hib_je, '+table[0]+'_hib_eval, '+table[0]+'_hib_time, '+table[0]+'_cluster_f FROM '+table[1]+' WHERE '+table[0]+'_cluster_f >= '+nclust+' AND '+table[0]+'_type = '+str(i+1)+' ORDER BY '+table[0]+'_hib_fo DESC LIMIT 1'
+        consulta = 'SELECT ROUND(SUM( (tabla.'+table[0]+'_hib_db - averages.averageDB) * (tabla.'+table[0]+'_hib_db - averages.averageDB) )/ (COUNT(tabla.'+table[0]+'_hib_db) - 1),4), averages.averageDB,  averages.averageE, averages.averageT, averages.averageC, COUNT(tabla.'+table[0]+'_hib_db) FROM '+table[1]+' AS tabla, (SELECT ROUND(AVG('+table[0]+'_hib_db),4) AS averageDB, ROUND(AVG('+table[0]+'_hib_eval),4) AS averageE, ROUND(AVG('+table[0]+'_hib_time),4) AS averageT, ROUND(AVG('+table[0]+'_cluster_f),4) AS averageC FROM '+table[1]+' WHERE '+table[0]+'_type = '+str(i+1)+' AND '+table[0]+'_cluster_f >= '+nclust+') AS averages WHERE '+table[0]+'_type = '+str(i+1)
+        statistics.execute(consulta)
+        consulta = 'SELECT '+table[0]+'_hib_db, '+table[0]+'_hib_eval, '+table[0]+'_hib_time, '+table[0]+'_cluster_f FROM '+table[1]+' WHERE '+table[0]+'_cluster_f >= '+nclust+' AND '+table[0]+'_type = '+str(i+1)+' ORDER BY '+table[0]+'_hib_fo DESC LIMIT 1'
         maxs.execute(consulta)
-        consulta = 'SELECT '+table[0]+'_hib_fo, '+table[0]+'_hib_db, '+table[0]+'_hib_je, '+table[0]+'_hib_eval, '+table[0]+'_hib_time, '+table[0]+'_cluster_f FROM '+table[1]+' WHERE '+table[0]+'_cluster_f >= '+nclust+' AND '+table[0]+'_type = '+str(i+1)+' ORDER BY '+table[0]+'_hib_fo ASC LIMIT 1'
+        consulta = 'SELECT '+table[0]+'_hib_db, '+table[0]+'_hib_eval, '+table[0]+'_hib_time, '+table[0]+'_cluster_f FROM '+table[1]+' WHERE '+table[0]+'_cluster_f >= '+nclust+' AND '+table[0]+'_type = '+str(i+1)+' ORDER BY '+table[0]+'_hib_fo ASC LIMIT 1'
         mins.execute(consulta)
-
-
         consulta = 'SELECT '+parametros[1]+' FROM '+table[1]+', '+table[1]+'p WHERE '+table[0]+'_type = '+table[0]+'p_id AND '+table[0]+'_type = '+str(i+1)+' AND '+table[0]+'_cluster_f >= '+nclust
         params.execute(consulta)
 
-        rowavg = avg.fetchone()
+        rowstatistics = statistics.fetchone()
         rowmaxs = maxs.fetchone()
         rowmins = mins.fetchone()
         rowparams = params.fetchone()
 
-        latexTable[i][0] = rowmaxs[0]
-        latexTable[i][1] = rowmaxs[2]
+        latexTable[i][0] = rowstatistics[0]
 
-        latexTable[i][2] +='            Promedio  & '+str(rowavg[0])+' & '+str(rowavg[1])+' & '+str(rowavg[2])+' & '+str(rowavg[3])+' & '+str(rowavg[4])+' & '+str(rowavg[5])+genSpaces(parametros[2])+'\\\\\n'
-        latexTable[i][2] +='            \\cline{1-7}\n'
-        latexTable[i][2] +='            Mejor & '+str(rowmaxs[0])+' & '+str(rowmaxs[1])+'  & '+str(rowmaxs[2])+' & '+str(rowmaxs[3])+' & '+str(rowmaxs[4])+' & '+str(rowmaxs[5])+' & '+genParams(rowparams)+'\\\\\n'
-        latexTable[i][2] +='            \\cline{1-7}\n'
-        latexTable[i][2] +='            Peor & '+str(rowmins[0])+' & '+str(rowmins[1])+'  & '+str(rowmins[2])+' & '+str(rowmins[3])+' & '+str(rowmins[4])+' & '+str(rowmins[5])+genSpaces(parametros[2])+'\\\\\n'
+        interval = (Decimal('1.959964') * (Decimal(str(rowstatistics[0])) / Decimal(str(rowstatistics[5]))).sqrt()).quantize(Decimal('1.0000'))
+
+        latexTable[i][1] +='            Promedio  & $' +str(rowstatistics[1])+' \\pm ' + str(interval)+ '$ & '+str(rowstatistics[2])+' & '+str(rowstatistics[3])+' & '+str(rowstatistics[4])+genSpaces(parametros[2])+'\\\\\n'
+        latexTable[i][1] +='            \\cline{1-5}\n'
+        latexTable[i][1] +='            Mejor & '+str(rowmaxs[0])+' & '+str(rowmaxs[1])+'  & '+str(rowmaxs[2])+' & '+str(rowmaxs[3])+' & '+genParams(rowparams)+'\\\\\n'
+        latexTable[i][1] +='            \\cline{1-5}\n'
+        latexTable[i][1] +='            Peor & '+str(rowmins[0])+' & '+str(rowmins[1])+'  & '+str(rowmins[2])+' & '+str(rowmins[3])+genSpaces(parametros[2])+'\\\\\n'
     
 
-        latexTable[i][2] +='        \\hline\n'
-        latexTable[i][2] +='        \\hline\n'
+        latexTable[i][1] +='        \\hline\n'
+        latexTable[i][1] +='        \\hline\n'
 
 
 if __name__ == "__main__":
@@ -95,8 +77,8 @@ if __name__ == "__main__":
     algsinfo = [['GAL','5'],['GAP','4'],['GAC','4'],['BEEL','5'],['BEEP','4'],['BEEC','4']]
     tandas = [['5','7','9','11'],['4','6','7','11'],['4','5','7','11'],['5'],['5'],['4','5','11']]
     table = [['ga','genetico'],['bee','abeja'],]
-    parametros = [['$I$ & $tt$ & $pc$ & $pm$ ','gap_i, gap_tt, gap_pc, gap_pm',4],
-                  ['$I$ & $m$ & $e$ & $eb$ & $ob$ ','beep_i, beep_m, beep_e, beep_eb, beep_ob',5]]
+    parametros = [['$I$ & $tt$ & $pc$ & $pm$ ','ROUND(gap_i,4), ROUND(gap_tt,4), ROUND(gap_pc,4), ROUND(gap_pm,4)',4],
+                  ['$I$ & $m$ & $e$ & $eb$ & $ob$ ','ROUND(beep_i,4), ROUND(beep_m,4), ROUND(beep_e,4), ROUND(beep_eb,4), ROUND(beep_ob,4)',5]]
 
     tn = 0
 
@@ -108,23 +90,23 @@ if __name__ == "__main__":
             conn = sqlite3.connect(algi[0]+str(tanda)+'.sql')
             tableout = open('table'+algi[0]+str(tanda)+'.tex', 'w')
 
-            latexTable = [[0.0,0.0,''] for i in range(20)]
+            latexTable = [[0.0,''] for i in range(20)]
             init = ''
             initC = ''
             finish = ''
             finishC = ''
 
-            avg = conn.cursor()
+            statistics = conn.cursor()
             maxs = conn.cursor()
             mins = conn.cursor()
             params = conn.cursor()
 
             init +='\\begin{table}[h!]\n    \\footnotesize\n    \\begin{center}\n        \\begin{tabular}{'+genCols(parametros[tn/3][2])+'}\n        \\hline\n'
-            init +='             & {\\bf FO} & {\\bf DB} & $J_e$ & {\\bf E} & {\\bf T} & {\\bf KE} & '+parametros[tn/3][0]+'\\\\\n'
+            init +='             & {\\bf DB} & {\\bf E} & {\\bf T} & {\\bf KE} & '+parametros[tn/3][0]+'\\\\\n'
             init +='        \\hline\n'
             init +='        \\hline\n'
 
-            takeOut(table[tn/3], parametros[tn/3], algi[1], avg, maxs, mins, params, latexTable, 0, 15)
+            takeOut(table[tn/3], parametros[tn/3], algi[1], statistics, maxs, mins, params, latexTable, 0, 15)
 
             finish +='        \\end{tabular}\n'
             if tn%3 == 0:
@@ -138,11 +120,11 @@ if __name__ == "__main__":
             finish +='\\end{table}\n'
 
             initC +='\\begin{table}[h!]\n    \\footnotesize\n    \\begin{center}\n        \\begin{tabular}{'+genCols(parametros[tn/3][2])+'}\n        \\hline\n'
-            initC +='             & {\\bf FO} & {\\bf DB} & $J_e$ & {\\bf E} & {\\bf T} & {\\bf KE} & '+parametros[tn/3][0]+'\\\\\n'
+            initC +='             & {\\bf DB} & {\\bf E} & {\\bf T} & {\\bf KE} & '+parametros[tn/3][0]+'\\\\\n'
             initC +='        \\hline\n'
             initC +='        \\hline\n'
 
-            takeOut(table[tn/3], parametros[tn/3], algi[1], avg, maxs, mins, params, latexTable, 15, 20)
+            takeOut(table[tn/3], parametros[tn/3], algi[1], statistics, maxs, mins, params, latexTable, 15, 20)
 
             finishC +='        \\end{tabular}\n'
             if tn%3 == 0:
@@ -161,18 +143,18 @@ if __name__ == "__main__":
 
             for i in range(15):
                 if i == 14:
-                    tableout.write(latexTable[i][2][0:len(latexTable[i][2])-15])
+                    tableout.write(latexTable[i][1][0:len(latexTable[i][1])-15])
                 else:
-                    tableout.write(latexTable[i][2])
+                    tableout.write(latexTable[i][1])
 
             tableout.write(finish)
 
             tableout.write(initC)
             for i in range(15,20):
                 if i == 19:
-                    tableout.write(latexTable[i][2][0:len(latexTable[i][2])-15])
+                    tableout.write(latexTable[i][1][0:len(latexTable[i][1])-15])
                 else:
-                    tableout.write(latexTable[i][2])
+                    tableout.write(latexTable[i][1])
 
 
             tableout.write(finishC)
